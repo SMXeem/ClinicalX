e81e617f53e1108195068fb952cb355acef2fe99
smxeem <sayedxeem@gmail.com>
2021-02-01 00:49:19 +0600

    booking doctor

--- ClinicalX/Models/ClinicalX.Designer.cs
+++ ClinicalX/Models/ClinicalX.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'H:\Project\ASP.NET\ClinicalX\ClinicalX\ClinicalX\Models\ClinicalX.edmx'. 
+﻿// T4 code generation is enabled for model 'F:\Xeem\ClinicalX\ClinicalX\Models\ClinicalX.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
